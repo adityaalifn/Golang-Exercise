a7a37a0677f517e28757d89e232b024658f18a80
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-09 05:22:44 +0700

    day 3 - map

---
+++ map_exercise/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var colors map[string]string
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	// colors["white"] = "#ffffff"
+	// delete(colors, "green")
+
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
