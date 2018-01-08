8e00b4c14c5af249955b4966c27375fbc6c64cb8
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-09 05:45:45 +0700

    day 3 - map - interface

---
+++ interface_exercise/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct {}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (sb spanishBot) getGreeting()  string{
+	return "Hola!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(eb.getGreeting())
+}
