10ac1b38377dbd4c6bc48629ff0f5bf16966021f
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-15 22:35:37 +0700

    day 6 - reader - writer - channels

--- http_exercise/main.go
+++ http_exercise/main.go
@@ -7,13 +7,24 @@ import (
 	"io"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	//return 1, nil
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
