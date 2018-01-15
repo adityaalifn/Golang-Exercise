98970b5c0c56063ca214f04818f8baf389c2c00d
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-15 22:49:25 +0700

    day 6 - serial - channels

--- channels_exercise/main.go
+++ channels_exercise/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
 func main() {
-	
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://bukalapak.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string)  {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is up!")
 }
