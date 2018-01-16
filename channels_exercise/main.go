7ee94ac7df1f5bcebbef37ea21f37bfa35fa9fe0
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-16 08:54:02 +0700

    day 7 - go routines - channels

--- channels_exercise/main.go
+++ channels_exercise/main.go
@@ -29,8 +29,8 @@ func main() {
 func checkLink(link string, c chan string)  {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, "might be down!")
-		c <- "might be down"
+		//fmt.Println(link, "might be down!")
+		c <- link + "might be down"
 		return
 	}
 	//fmt.Println(link, "is up!")
