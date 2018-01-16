481286bcd1f4953ff759808cebf94975804b6977
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-16 08:53:09 +0700

    day 6 - go routines - channels

--- channels_exercise/main.go
+++ channels_exercise/main.go
@@ -10,19 +10,29 @@ func main() {
 		"http://google.com",
 		"http://facebook.com",
 		"http://golang.org",
+		"http://tokopedia.com",
 		"http://bukalapak.com",
 	}
 
+	c := make(chan string, len(links))
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
 }
 
-func checkLink(link string)  {
+
+func checkLink(link string, c chan string)  {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "might be down"
 		return
 	}
-	fmt.Println(link, "is up!")
+	//fmt.Println(link, "is up!")
+	c <- link + " is up"
 }
