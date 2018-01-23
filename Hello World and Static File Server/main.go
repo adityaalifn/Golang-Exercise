3f682476443e9567e6c8425f836a5ee7eb2342a8
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-23 13:47:23 +0700

    day 9 - Hello World and Static File Server

--- Hello World and Static File Server/main.go
+++ Hello World and Static File Server/main.go
@@ -1,1 +1,22 @@
-package Hello_World_and_Static_File_Server
+package main
+
+import (
+	"flag"
+	"net/http"
+	"log"
+)
+
+func main() {
+	//var input string
+	//fmt.Print("Enter Your Name: ")
+	//fmt.Scanln(&input)
+	//fmt.Println("Hello, %s!", input)
+
+	port := flag.String("p", "8000", "port")
+	dir := flag.String("d", ".", "dir")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Serving %s on Http port: %s\n", *dir, *port)
+	log.Fatal(http.ListenAndServe(":" + *port, nil))
+}
