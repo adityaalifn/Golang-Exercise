7f52e3b0f2d902dbde0c1d4c2e79084e1ac54b83
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-25 23:40:43 +0700

    day 11 : starting sentdex channel tutorial https://www.youtube.com/watch?v=G3PvTWRIhZA&list=PLQVvvaa0QuDeF3hP0wQoSxpkqgRcgxMqX - intro - syntax - types

--- Hello World and Static File Server/gotut.go
+++ Hello World and Static File Server/gotut.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import ("fmt")
+
+//func foo()  {
+//	fmt.Println("The square root of 4 is", math.Sqrt(4))
+//	//foo()
+//}
+
+func add(x float64, y float64) float64 {
+	return x+y
+}
+
+func multiple(a, b string) (string,string) {
+	return a,b
+}
 
 func main() {
-	fmt.Println("Welcome to Go!")
+	//fmt.Println("A number from 1-100", rand.Intn(100))
+
+	/*var num1,num2 float64 = 5.6,2.7
+	var num2 float64 = 2.7*/
+
+	num1,num2  := 5.6,2.7
+	fmt.Println(add(num1,num2))
+
+	w1, w2 := "hey", "there"
+	fmt.Println(multiple(w1,w2))
+
+	var a int = 62
+	var b float64 = float64(a)
+	fmt.Println(b)
 }
