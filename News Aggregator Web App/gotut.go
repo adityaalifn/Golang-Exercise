58dbdcaad68a1dfd437091211389fbfc996403df
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-26 15:24:24 +0700

    day 12 : starting sentdex channel tutorial https://www.youtube.com/watch?v=G3PvTWRIhZA&list=PLQVvvaa0QuDeF3hP0wQoSxpkqgRcgxMqX - intro - syntax - types - pointer - HandleFunc and Serve - struct - method (value&pointer receiver) - xml parsing

--- News Aggregator Web App/gotut.go
+++ News Aggregator Web App/gotut.go
@@ -8,24 +8,29 @@ import (
 )
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
-}
 
 func main() {
+	var s SitemapIndex
+	var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 
-	fmt.Println(s.Locations)
+	//fmt.Println(s.Locations)
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		fmt.Println(n)
+	}
 }
---
+++ loop_exercise/gotut.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	i := 1
+	for i < 10{
+		i++
+	}
+
+	for{
+
+	}
+}
