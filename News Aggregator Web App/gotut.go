f7ea4141655509cec7b6f3297dfe902ea8380e2c
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-26 16:08:02 +0700

    day 12 : starting sentdex channel tutorial https://www.youtube.com/watch?v=G3PvTWRIhZA&list=PLQVvvaa0QuDeF3hP0wQoSxpkqgRcgxMqX - intro - syntax - types - pointer - HandleFunc and Serve - struct - method (value&pointer receiver) - xml parsing - looping - maps

--- News Aggregator Web App/gotut.go
+++ News Aggregator Web App/gotut.go
@@ -13,14 +13,19 @@ type SitemapIndex struct {
 
 type News struct {
 	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
+	Keywords []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
 }
 
+type NewsMap struct {
+	Keyword string
+	Location string
+}
 
 func main() {
 	var s SitemapIndex
 	var n News
+	news_map := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
@@ -31,6 +36,15 @@ func main() {
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
-		fmt.Println(n)
+
+		for idx, _ := range n.Titles{
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
 	}
 }
