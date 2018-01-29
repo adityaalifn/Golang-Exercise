6d27baae936789f036cc906429030962ca4e7f76
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-29 09:44:12 +0700

    day 14 : sentdex channel tutorial https://www.youtube.com/watch?v=G3PvTWRIhZA&list=PLQVvvaa0QuDeF3hP0wQoSxpkqgRcgxMqX - intro - syntax - types - pointer - HandleFunc and Serve - struct - method (value&pointer receiver) - xml parsing - looping - maps - HTML template - finished the web - goroutines and sync - defer - panic and recover - channel and buffering - adding concurency on web

--- Simple Web App/gotut.go
+++ Simple Web App/gotut.go
@@ -6,7 +6,10 @@ import (
 	"html/template"
 	"io/ioutil"
 	"encoding/xml"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type NewsAggPage struct {
 	Title string
@@ -28,22 +31,39 @@ type NewsMap struct {
 	Location string
 }
 
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+
+	var n News
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+
+	c <- n
+}
+
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s SitemapIndex
-	var n News
-	news_map := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
-	//fmt.Println(s.Locations)
-	for _, Location := range s.Locations{
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
+	news_map := make(map[string]NewsMap)
+	resp.Body.Close()
+	queue := make(chan News, 30)
 
-		for idx, _ := range n.Titles{
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+	for _, Location := range s.Locations{
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+
+	wg.Wait()
+	close(queue)
+
+	for elem := range queue {
+		for idx, _ := range elem.Titles {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
 
